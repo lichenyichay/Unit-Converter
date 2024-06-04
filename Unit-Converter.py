5099d5738f0dbac1b2d1b9caa2cc20e38f797dce
lichenyichay <lichenyi_2020@qq.com>
2024-06-04 22:33:42 +0800

    完成基本框架（窗口）

--- Unit-Converter.py
+++ Unit-Converter.py
@@ -1,3 +1,13 @@
 # Author:Chay
 # Time:2024/6/4 22:09
 # Release:Alpha 1.0.0
+
+from tkinter import *
+top = Tk()
+top.geometry('750x750')
+top.title("单位换算器")
+label = Label(text="单位换算器", font=("微软雅黑", 25), anchor="n")
+label.pack()
+
+
+top.mainloop()
