45f1153d304849abff1d59e9a7febe0acc983953
chay <lichenyi_2020@qq.com>
2024-09-08 11:09:19 +0800

    初始界面

--- Unit-Converter.py
+++ Unit-Converter.py
@@ -3,11 +3,16 @@
 # Release:Alpha 1.0.0
 
 from tkinter import *
+from UnitConvertChay import *
 top = Tk()
 top.geometry('750x750')
 top.title("单位换算器")
 label = Label(text="单位换算器", font=("微软雅黑", 25), anchor="n")
 label.pack()
 
+button1=Button(top,text="单位换算")
+button1.place(width=200,height=200,x=50,y=200)
+button2=Button(top,text="物理量计算")
+button2.place(width=200,height=200,x=500,y=200)
 
 top.mainloop()
